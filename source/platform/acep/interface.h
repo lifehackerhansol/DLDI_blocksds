6f5a16a1b366739fa42b35726320147d9198fa00
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-25 21:12:53 -0700

    dldi_header: allow modifying IO_INTERFACE->features
    1. We cannot always assume the driver is for Slot-1 only
    2. We cannot assume that both reads and writes will be available
    
    So just add another define in interface.h.

--- source/platform/acep/interface.h
+++ source/platform/acep/interface.h
@@ -9,5 +9,9 @@
 
 #pragma once
 
+#include <nds/arm9/dldi_asm.h>
+
 #define DRIVER_IOTYPE "ACEP"
 #define DRIVER_NAME "Ace3DS+"
+
+#define DRIVER_FEATURES (FEATURE_MEDIUM_CANREAD | FEATURE_MEDIUM_CANWRITE | FEATURE_SLOT_NDS)
